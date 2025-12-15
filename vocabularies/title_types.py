d72086f721a22a70fe5a873dc607710a82ed3262
Ian W. Scott <scottianw@gmail.com>
2025-12-15 16:54:08 -0500

    Merge stats-dashboard feature branch (dev/stats) into main.
    This merge commit brings with it:
    
    * The stats-dashboard submodule with global and community
      statistics dashboards, as well as infrastructure for
      tracking the necessary aggregated statistics.
    * Integration of the test suites for invenio-stats-dashboard
      and invenio-record-importer-kcworks with the main
      project test suite.
    * Major updates and reorganization of the test suite.
    * Other minor tweaks to the ui for the record detail page,
      user dashboard, search results, and upload form.
    * Improvements to the invenio-record-importer-kcworks
      submodule, especially around updating of "created" dates.
    * Several new CLI commands, not only for the community
      stats, but also for various record and user management
      actions.
    
    Full details are listed in docs/source/changelog.md

--- vocabularies/title_types.py
+++ vocabularies/title_types.py
@@ -5,69 +5,62 @@
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
-"""Vocabulary pytest fixtures for languages."""
+"""Vocabulary pytest fixtures for date types."""
 
 import pytest
 from invenio_access.permissions import system_identity
-from invenio_search import current_search_client
+from invenio_search.proxies import current_search_client
 from invenio_vocabularies.proxies import current_service as vocabulary_service
 from invenio_vocabularies.records.api import Vocabulary
 from invenio_vocabularies.records.models import VocabularyMetadata
 
 
 @pytest.fixture(scope="module")
-def language_type(app):
-    """Fixture to create the language vocabulary type.
+def title_type_type(app):
+    """Fixture to create the title type vocabulary type.
 
     Returns:
-        VocabularyType: The created language vocabulary type.
+        VocabularyType: The created title type vocabulary type.
     """
-    return vocabulary_service.create_type(system_identity, "languages", "lng")
+    return vocabulary_service.create_type(system_identity, "titletypes", "ttyp")
 
 
-language_data = [
+title_type_data = [
     {
-        "id": "eng",
-        "title": {"en": "English"},
-        "type": "languages",
+        "id": "subtitle",
+        "props": {"datacite": "Subtitle"},
+        "title": {"en": "Subtitle"},
+        "type": "titletypes",
     },
     {
-        "id": "fra",
-        "title": {"en": "French"},
-        "type": "languages",
+        "id": "alternative-title",
+        "props": {"datacite": "AlternativeTitle"},
+        "title": {"en": "Alternative title"},
     },
     {
-        "id": "spa",
-        "title": {"en": "Spanish"},
-        "type": "languages",
+        "id": "translated-title",
+        "props": {"datacite": "TranslatedTitle"},
+        "title": {"en": "Translated title"},
     },
     {
-        "id": "heb",
-        "title": {"en": "Hebrew"},
-        "type": "languages",
-    },
-    {
-        "id": "por",
-        "title": {"en": "Portuguese"},
-        "type": "languages",
+        "id": "other",
+        "props": {"datacite": "Other"},
+        "title": {"en": "Other"},
     },
 ]
 
 
 @pytest.fixture(scope="module")
-def language_v(app, language_type):
-    """Fixture to create the language vocabulary records."""
-    for language in language_data:
-        vocabulary_service.create(
-            system_identity,
-            language,
-        )
+def title_type_v(app, title_type_type):
+    """Title Type vocabulary record."""
+    for title_type in title_type_data:
+        vocabulary_service.create(system_identity, {**title_type, "type": "titletypes"})
 
     Vocabulary.index.refresh()
 
 
 @pytest.fixture(scope="function")
-def reindex_languages(running_app):
+def reindex_title_types(running_app):
     """Ensure vocabulary search indices exist and are populated.
 
     This method checks if vocabulary indices are missing or empty and
@@ -81,16 +74,16 @@ def reindex_languages(running_app):
     if not search_client.indices.exists(index=index_name):
         Vocabulary.index.create()
 
-    # First check if the vocabulary type 'languages' exists
+    # First check if the vocabulary type 'titletypes' exists
     type_search = search_client.search(
         index=index_name,
-        body={"query": {"term": {"id": "languages"}}, "size": 1},
+        body={"query": {"term": {"id": "titletypes"}}, "size": 1},
     )
 
     # Then check if it has vocabulary term records
     terms_search = search_client.search(
         index=index_name,
-        body={"query": {"term": {"type": "languages"}}, "size": 1},
+        body={"query": {"term": {"type": "titletypes"}}, "size": 1},
     )
 
     if (
@@ -100,7 +93,7 @@ def reindex_languages(running_app):
         db_records = VocabularyMetadata.query.filter(
             VocabularyMetadata.json.op("->")("type")
             .op("->>")("id")
-            .in_(["languages", "lng"])
+            .in_(["titletypes", "ttyp"])
         ).all()
 
         if db_records:
